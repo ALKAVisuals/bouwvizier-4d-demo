b7e3e9525d3507576209980a28150af31f5e552a
ALKAGroup <info@alkavisuals.nl>
2026-08-02 19:38:16 +0200

    Generate static project routes for GitHub Pages

--- app/dashboard/projects/[id]/page.tsx
+++ app/dashboard/projects/[id]/page.tsx
@@ -1,6 +1,12 @@
 import { ProjectPageShell } from "@/components/dashboard/ProjectPageShell";
 import { FourDWorkspace } from "@/components/fourd/FourDWorkspace";
 
+const projectIds = ["havenkwartier", "utrecht-kantoor", "dc-nijmegen", "school-eindhoven"];
+
+export function generateStaticParams() {
+  return projectIds.map((id) => ({ id }));
+}
+
 export default function ProjectDashboardPage() {
   return <ProjectPageShell><FourDWorkspace /></ProjectPageShell>;
 }
